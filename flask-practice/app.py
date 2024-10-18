975fecb033c705805f048110e4c425b2f32ce3ce
stepannovoselov <105072946+stepannovoselov@users.noreply.github.com>
2024-10-18 16:15:47 +0300

    Update app.py

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -4,16 +4,19 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+# Когда пользователь в браузере наберет АДРЕСХОСТА/ (зайдет на главную страницу сайта), вызывается эта функция
 @app.route('/')
 def home():
     return 'Hello, World!'
 
 
+# Когда пользователь в браузере наберет АДРЕСХОСТА/greet/любое-значение, вызывается эта функция и передаёт в переменную name значение, указанное пользователем после greet/
 @app.route('/greet/<name>')
 def greet(name):
     return f'Hello, {name}!'
 
 
+# Когда пользователь в браузере наберет АДРЕСХОСТА/time, вызывается эта функция, обработает html страницу, добавив в неё значение переменной local_time, и отправит пользователю
 @app.route('/time')
 def get_time():
     return render_template(f'time.html', local_time=datetime.now())
